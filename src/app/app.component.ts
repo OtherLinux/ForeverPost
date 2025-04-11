1fbfc78c551ad59119e68a5098b83378371a4afa
OtherLinux <git@cuprum.yu>
2025-04-11 17:42:59 +0200

    Added random post functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import {RouterOutlet, RouterLink, RouterLinkActive} from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {RouterOutlet, RouterLink, RouterLinkActive, RedirectCommand, Router} from '@angular/router';
 import {NgOptimizedImage} from '@angular/common';
 import {PostsComponent} from './Pages/posts/posts.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,18 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 export class AppComponent {
   title = 'Forever Post';
   logoSrc = './img.png';
+
+  private router = inject(Router)
+  private http = inject(HttpClient)
+
+  getRandomPost() {
+    const newestid = this.http.get<{ newestMessageId: number }>(`https://api.cuprum.uk/api/newestid`).subscribe(
+      res => {
+        if (res === null) {
+          return;
+        }
+        this.router.navigate(['/search/' + Math.floor(Math.random() * (Math.floor(res.newestMessageId) - Math.ceil(1)) + Math.ceil(1))]);
+      },
+    );
+  };
 }
