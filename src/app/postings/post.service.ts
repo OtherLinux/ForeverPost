59d7a9b0ec20a0f78e538f5555b8deb834898687
OtherLinux <git@cuprum.yu>
2025-04-16 19:54:23 +0200

    Fixed bug, with post.service.ts

--- src/app/postings/post.service.ts
+++ src/app/postings/post.service.ts
@@ -11,7 +11,7 @@ export class PostService {
   private newestid: number = 0; //is used to load new content
   private fetching = false; //is used to prevent double loading
   private updating = false; //is used to prevent double loading
-  private debug = true
+  private debug = false;
   private backendUrl = this.debug ? 'http://127.0.0.1:8000/api/' : 'https://api.cuprum.uk/api/';
   public posts: PostData[] = []
   public found_post: PostData = {
